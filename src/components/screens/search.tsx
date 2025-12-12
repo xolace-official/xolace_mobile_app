5c851023f92930788b032ae647ecb858579ccc04
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-12 16:39:13 +0000

    Update search.tsx

--- src/components/screens/search.tsx
+++ src/components/screens/search.tsx
@@ -2,5 +2,5 @@ import { FC } from "react";
 import { View } from "react-native";
 
 export const Search: FC = () => {
-  return <View className="flex-1 bg-linear-back" />;
+  return <View className="flex-1 bg-background" />;
 };
