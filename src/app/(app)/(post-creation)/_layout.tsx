1ba6ec572115cb78d877e3e35a6bd215ac2a5c6e
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-15 14:02:36 +0000

    feat:add post-to page

--- src/app/(app)/(post-creation)/_layout.tsx
+++ src/app/(app)/(post-creation)/_layout.tsx
@@ -4,6 +4,10 @@ export default function PostCreationLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ presentation: "card" , headerShown: false}} />
+      <Stack.Screen
+        name="post-to"
+        options={{ presentation: 'pageSheet', headerShown: false }}
+      />
     </Stack>
   );
 }
---
+++ src/components/screens/post-to-screen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export const PostToScreen = () => {
+  return (
+    <View>
+      <Text>PostToScreen</Text>
+    </View>
+  )
+}
